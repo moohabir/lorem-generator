7e6590d772d66a476e71e7354fb6e912b1b318f4
moohabir <87937848+moohabir@users.noreply.github.com>
2022-07-21 21:55:21 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import "./styles.css";
-
+import data from "./data";
+import { useState } from "react";
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+    if (amount <= 0) {
+      amount = 1;
+    }
+    if (amount > 10) {
+      amount = 10;
+    }
+
+    setList(data.slice(0, amount));
+    console.log(amount);
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs</label>
+        <input
+          type="number"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button>Generate Text</button>
+      </form>
+      {list.map((item, index) => (
+        <article key={index}>
+          <p>{item}</p>
+        </article>
+      ))}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+  "hjhjhhlhk dsfdsfsfsfs paragaraph1",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph2",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph3",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph4",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph5",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph6",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph7",
+  "hjhjhhlhk dsfdsfsfsfs paragaraph8",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph9",
+
+  "hjhjhhlhk dsfdsfsfsfs paragaraph10"
+];
